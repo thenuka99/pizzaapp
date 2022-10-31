a974e92a5983a80f3440072700fe2da851693d72
thenuka99 <janithherath2@gmail.com>
2022-10-31 10:40:19 +0530

    first commit

---
+++ src/test/java/io/moraead/pizzaapp/PizzaappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.moraead.pizzaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
