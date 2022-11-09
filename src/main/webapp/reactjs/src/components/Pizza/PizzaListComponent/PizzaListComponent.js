cdfbca390131375ce4a3e82602af36af1dcfa153
thenuka99 <janithherath2@gmail.com>
2022-11-10 00:10:29 +0530

    pizza shop #3

--- src/main/webapp/reactjs/src/components/Pizza/PizzaListComponent/PizzaListComponent.js
+++ src/main/webapp/reactjs/src/components/Pizza/PizzaListComponent/PizzaListComponent.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './EmployeeListComponent.css';
+import { Link } from 'react-router-dom';
+import { deletepizza, loadpizzas} from '../../../services/AuthService';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const PizzaListComponent = () => {
+  //useStates
+  const [pizzas, setPizzas] = useState([]);    
+
+    
+  useEffect(() => {
+    LoadPizzas();
+  }, []);
+
+  const LoadPizzas = async () => {
+
+      try {
+          const response = await loadpizzas();
+          console.log(response.data);
+          setPizzas(response.data);
+      } catch (e) {
+          console.log(e);
+      }
+  };
+
+  const Deletepizza = async(id) => {
+      try {
+          
+          const response = await deletepizza(id);
+          console.log('item deleted');
+          console.log(response);
+          LoadPizzas();
+      } catch (e) {
+          console.log(e);
+      }
+  };
+ 
+
   return (
-    <div>PizzaListComponent</div>
-  )
-}
+      <div className='category_container'>
+          <button>
+          <a href="/addpizza"> Add Employee</a>
+          </button>
+
+          <table>
+              <thead>
+                  <tr>
+                      <th scope="col">Title</th>
+                      <th scope="col">price</th>
+                      <th scope="col">veg</th>
+                      <th scope="col">Edit</th>
+                      <th scope="col">Delete</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {
+                      pizzas.map((pizza) => (
+                          <tr key={pizza.pizzaId}>
+                              <td data-label="Title"><h4> {pizza.title}</h4></td>
+                              <td data-label="price"><h4>Rs.{pizza.price} </h4></td>
+                              <td data-label="vegetarian"><h4>{pizza.veg?"yes":"no"} </h4></td>
+                              <td data-label="Edit" ><Link to={`/updatepizza/${pizza.pizzaId}`}><EditIcon/></Link> </td>
+                              <td data-label="Delete"><DeleteIcon  onClick={() => Deletepizza(pizza.pizzaId)}/> </td>
+                          </tr>
+                      ))
+                  }
+              </tbody>
+          </table>
+      </div>
+  );
+};
 
 export default PizzaListComponent
--- src/main/webapp/reactjs/src/components/Shop/ShopListComponent/ShopListComponent.js
+++ src/main/webapp/reactjs/src/components/Shop/ShopListComponent/ShopListComponent.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './EmployeeListComponent.css';
+import { Link } from 'react-router-dom';
+import { deleteshop, loadshops} from '../../../services/AuthService';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const ShopListComponent = () => {
+  //useStates
+  const [shops, setShops] = useState([]);    
+
+    
+  useEffect(() => {
+      LoadShops();
+  }, []);
+
+  const LoadShops = async () => {
+
+      try {
+          const response = await loadshops();
+          console.log(response.data);
+          setShops(response.data);
+      } catch (e) {
+          console.log(e);
+      }
+  };
+
+  const Deleteemployee = async(id) => {
+      try {
+          
+          const response = await deleteshop(id);
+          console.log('item deleted');
+          console.log(response);
+          LoadShops();
+      } catch (e) {
+          console.log(e);
+      }
+  };
+ 
+
   return (
-    <div>ShopListComponent</div>
-  )
-}
+      <div className='category_container'>
+          <button>
+          <a href="/addshop"> Add Shop</a>
+          </button>
+
+          <table>
+              <thead>
+                  <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Address</th>
+                      <th scope="col">Contact Number</th>
+                      <th scope="col">Edit</th>
+                      <th scope="col">Delete</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {
+                      shops.map((shop) => (
+                          <tr key={shop.shopId}>
+                              <td data-label="Name"><h4> {shop.name}</h4></td>
+                              <td data-label="Address"><h4>{shop.address} </h4></td>
+                              <td data-label="Contact Number"><h4>{shop.contactNo} </h4></td>
+                              <td data-label="Edit" ><Link to={`/updateshop/${shop.shopId}`}><EditIcon/></Link> </td>
+                              <td data-label="Delete"><DeleteIcon  onClick={() => Deleteemployee(shop.shopId)}/> </td>
+                          </tr>
+                      ))
+                  }
+              </tbody>
+          </table>
+      </div>
+  );
+};
 
 export default ShopListComponent;
