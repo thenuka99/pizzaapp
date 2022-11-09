f493de13c40b0f12f090f1c2779fc9a59c393dd4
thenuka99 <janithherath2@gmail.com>
2022-11-09 13:25:08 +0530

    change db

--- src/main/java/io/moraead/pizzaapp/config/MongoConfiguration.java
+++ src/main/java/io/moraead/pizzaapp/config/MongoConfiguration.java
@@ -1,22 +1,26 @@
 package io.moraead.pizzaapp.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
+@EnableMongoRepositories(basePackages = "io.moraead.pizzaapp")
 public class MongoConfiguration extends AbstractMongoClientConfiguration {
-
-    @Value("${port}")
-    private String port;
 
     @Value("${dbname}")
     private String dbname;
+
+    @Value("${connectdb}")
+    private String connectdb;
 
     @Override
     protected String getDatabaseName() {
@@ -25,11 +29,15 @@ public class MongoConfiguration extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        return MongoClients.create();
+        final ConnectionString connectionString = new ConnectionString(connectdb);
+        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
     }
 
     @Override
-    public MongoTemplate mongoTemplate(MongoDatabaseFactory databaseFactory, MappingMongoConverter converter) {
-        return new MongoTemplate(mongoClient(),getDatabaseName());
+    public Collection<String> getMappingBasePackages() {
+        return Collections.singleton("io.moraead.pizzaapp");
     }
 }
